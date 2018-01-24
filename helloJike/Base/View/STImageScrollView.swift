ab79350820cc8dbedc75f318236f7a30f26a8570
Mapleiny <i@maple.im>
2018-01-24 19:05:33 +0800

    图片缩放功能

---
+++ helloJike/Base/View/STImageScrollView.swift
@@ -0,0 +1,50 @@
+//
+//  STImageScrollView.swift
+//  helloJike
+//
+//  Created by Mapleiny on 2018/1/24.
+//  Copyright © 2018年 Maple Yin. All rights reserved.
+//
+
+import UIKit
+
+class STImageScrollView: UIView {
+    
+    private let imageView = ImageView()
+    private let scrollView = STScrollView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(scrollView)
+        scrollView.addSubview(imageView)
+        scrollView.contentInsetAdjustmentBehavior = .never
+        scrollView.delegate = self
+        scrollView.alwaysBounceVertical = true
+        scrollView.minimumZoomScale = 1
+        scrollView.maximumZoomScale = 5
+        scrollView.snp.makeConstraints { (make) in
+            make.edges.equalTo(self)
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func setup(image:UIImage) {
+        imageView.image = image
+        imageView.snp.remakeConstraints { (make) in
+            make.center.equalTo(scrollView)
+            make.width.equalTo(scrollView)
+            make.height.equalTo(imageView.snp.width).multipliedBy(image.size.height/image.size.width)
+        }
+    }
+}
+
+
+extension STImageScrollView : UIScrollViewDelegate {
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+}
--- helloJike/Base/View/STScrollView.swift
+++ helloJike/Base/View/STScrollView.swift
@@ -10,25 +10,35 @@ import UIKit
 
 class STScrollView: UIScrollView {
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    private var prevBoundsSize:CGSize = CGSize.zero
     
     override func layoutSubviews() {
         super.layoutSubviews()
-
+        updateZoomViewLayout()
     }
-}
-
-extension STScrollView {
-
-}
-
-extension STScrollView {
     
+    
+}
+
+// Zoom View Layout
+extension STScrollView {
+    private func updateZoomViewLayout() {
+     
+        if let viewForZooming = delegate?.viewForZooming,
+            let view = viewForZooming(self) {
+            
+            let contentWidth = bounds.width * zoomScale
+            let size = view.bounds.size
+            if size.width != 0 {
+                let imageHeight = size.height / size.width * contentWidth
+                let y = max(0.0,( bounds.height - imageHeight ) / 2)
+                view.frame = CGRect(x: 0, y: y, width: contentWidth, height: imageHeight)
+            }
+            
+            if !prevBoundsSize.equalTo(size) {
+                contentSize = view.frame.size
+                prevBoundsSize = size
+            }
+        }
+    }
 }
